597517be9203edd57f7b1ee9936e327be47c9953
manoj-2606 <manojmanojkumar2513@gmail.com>
2026-02-07 19:03:36 +0530

    files are modified

--- Terraform/Day2/locals.tf
+++ Terraform/Day2/locals.tf
@@ -1,16 +1,1 @@
-# Stop repeating naming logic.
 
-# Centralize.
-
-locals {
-  prefix = "mj"
-  env    = "dev"
-
-  vnet_name = "${local.prefix}-${local.env}-vnet"
-}
-
-# Why locals?
-
-# Because hardcoded strings across files = maintenance nightmare.
-
-# Professionals centralize naming.
--- Terraform/Day2/variables.tf
+++ Terraform/Day2/variables.tf
@@ -1,13 +1,1 @@
-# Stop hardcoding values. Everything dynamic.
 
-variable "location" {
-  default = "centralindia"
-}
-
-variable "storage_names" {
-  type = set(string)
-}
-
-variable "subnets" {
-  type = map(string)
-}
