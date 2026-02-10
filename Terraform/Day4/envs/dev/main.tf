45353fb20007a8a149d063cc6eb26e0c2d7fabf8
manoj-2606 <manojmanojkumar2513@gmail.com>
2026-02-10 12:51:52 +0530

    added .tf files

---
+++ Terraform/Day4/envs/dev/main.tf
@@ -0,0 +1,9 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "4985f681-bfb3-4e92-a131-b1e85dd4f934"
+}
+
+resource "azurerm_resource_group" "demo" {
+  name     = "rg-demo-test"
+  location = "eastus"
+}
